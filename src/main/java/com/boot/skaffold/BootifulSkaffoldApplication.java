ad4ad8666f06837d3a60c6f96db0cda8f4050de2
Roy <Nilanjan.Roy@emc.com>
2020-01-31 10:21:48 +0530

    initial commit

---
+++ src/main/java/com/boot/skaffold/BootifulSkaffoldApplication.java
@@ -0,0 +1,22 @@
+package com.boot.skaffold;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class BootifulSkaffoldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootifulSkaffoldApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String sayHello(){
+
+		return "Hello Spring boot dev ... Skaffolding  with Jib	";
+	}
+
+}
